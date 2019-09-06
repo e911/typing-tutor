5da2401203abe7ac367d40eb3fd7e449e97d4152
t_barun <pujanthapa88.63@gmail.com>
2019-09-06 22:51:29 +0545

    Updates

---
+++ precache-manifest.524d9bb51da51ec980bc140a36012d2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78ceefc15d021a6ebd3917de15f7914",
+    "url": "/typing-tutor/index.html"
+  },
+  {
+    "revision": "e325f039c0cd8e8545dc",
+    "url": "/typing-tutor/static/css/2.d32869f0.chunk.css"
+  },
+  {
+    "revision": "153575ae75045aafae5d",
+    "url": "/typing-tutor/static/css/main.5224a62e.chunk.css"
+  },
+  {
+    "revision": "e325f039c0cd8e8545dc",
+    "url": "/typing-tutor/static/js/2.85bba0ab.chunk.js"
+  },
+  {
+    "revision": "153575ae75045aafae5d",
+    "url": "/typing-tutor/static/js/main.e5f3fa39.chunk.js"
+  },
+  {
+    "revision": "969ba72c629b3f5db441",
+    "url": "/typing-tutor/static/js/runtime~main.a1f57c74.js"
+  }
+]);
